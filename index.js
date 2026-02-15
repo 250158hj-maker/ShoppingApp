2adfb6b676c0c5265de5cbddafadc9c40b61e55c
takumi <2501580hj@yse-c.net>
2026-02-15 15:57:51 +0900

    Mongoose Setting

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-// Required Package
+// 定数宣言
+const DB = "farmStand";
+const DB_CONNECT_MSG = "MongoDB connected!!";
+const PORT = 3000;
+
+// Required Package and Mongoose
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+mongoose
+    .connect(`mongodb://localhost:27017/${DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Success:", DB_CONNECT_MSG);
+    })
+    .catch((err) => {
+        console.log("Error:", err);
+    });
 
 // Express Settings
 app.set("views", path.join(__dirname), "views");
 app.set("view engine", "ejs");
 
-// 定数宣言
-const PORT = 3000;
+app.get("/dog", (req, res) => {
+    res.send("wanwan");
+});
 
 // Express Server Setup
 app.listen(PORT, () => {
