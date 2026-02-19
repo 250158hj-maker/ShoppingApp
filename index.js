cfd286d2c5365e5893857d077ec02c0433b4e4db
takumi <2501580hj@yse-c.net>
2026-02-19 20:16:55 +0900

    routing: 農場の一覧と新規作成ルーティングの実装

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 // 定数宣言
 const DB = "farmStandTake2";
-const PRODUCTS_ENDPOINT = "/products";
-const VIEWS_DIRECTORY = "products";
-const categories = ["野菜", "果物", "乳製品", "パン類"];
 const PORT = 3000;
+// Product
+const PRODUCTS_ENDPOINT = "/products";
+const VIEWS_PRODUCTS = "products";
+const categories = ["野菜", "果物", "乳製品", "パン類"];
+// Farm
+const FARMS_ENDPOINT = "/farms";
+const VIEWS_FARMS = "farms";
 
 // Required Package and Mongoose
 const express = require("express");
-const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Product = require("./models/product");
+const Farm = require("./models/farm");
 
+const app = express();
 mongoose
     .connect(`mongodb://localhost:27017/${DB}`, {
         useNewUrlParser: true,
@@ -31,15 +36,34 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+// 農場一覧表示
+app.get(FARMS_ENDPOINT, async (req, res) => {
+    const farms = await Farm.find();
+    res.render(`${VIEWS_FARMS}/index`, { farms });
+});
+
+// 新規農場作成ページ
+app.get(`${FARMS_ENDPOINT}/new`, (req, res) => {
+    res.render(`${VIEWS_FARMS}/new`);
+});
+
+// 農場のDB登録
+app.post(FARMS_ENDPOINT, async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    // 一覧ページへリダイレクト
+    res.redirect(FARMS_ENDPOINT);
+});
+
 // 一覧表示
 app.get(PRODUCTS_ENDPOINT, async (req, res) => {
     const { category } = req.query;
     if (category) {
         const products = await Product.find({ category });
-        res.render(`${VIEWS_DIRECTORY}/index`, { products, category });
+        res.render(`${VIEWS_PRODUCTS}/index`, { products, category });
     } else {
         const products = await Product.find();
-        res.render(`${VIEWS_DIRECTORY}/index`, {
+        res.render(`${VIEWS_PRODUCTS}/index`, {
             products,
             category: "All",
             categories,
@@ -49,7 +73,7 @@ app.get(PRODUCTS_ENDPOINT, async (req, res) => {
 
 // 新規商品作成ページへ
 app.get(`${PRODUCTS_ENDPOINT}/new`, (req, res) => {
-    res.render(`${VIEWS_DIRECTORY}/new`, { categories });
+    res.render(`${VIEWS_PRODUCTS}/new`, { categories });
 });
 
 // 新規商品登録
@@ -64,7 +88,7 @@ app.post(PRODUCTS_ENDPOINT, async (req, res) => {
 app.get(`${PRODUCTS_ENDPOINT}/:id/edit`, async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render(`${VIEWS_DIRECTORY}/edit`, { product, categories });
+    res.render(`${VIEWS_PRODUCTS}/edit`, { product, categories });
 });
 
 // 商品の編集
@@ -91,7 +115,7 @@ app.delete(`${PRODUCTS_ENDPOINT}/:id`, async (req, res) => {
 app.get(`${PRODUCTS_ENDPOINT}/:id`, async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render(`${VIEWS_DIRECTORY}/show`, { product });
+    res.render(`${VIEWS_PRODUCTS}/show`, { product });
 });
 
 // Express Server Setup
